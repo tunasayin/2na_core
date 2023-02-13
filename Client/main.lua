2f28d76bd0bee550cb808f51a55900fb344cd414
Tuna Sayin <65562418+tunasayin@users.noreply.github.com>
2023-02-13 03:32:45 +0300

    Added GetVehicleDamage & ApplyVehicleDamage functions

--- Client/main.lua
+++ Client/main.lua
@@ -26,6 +26,47 @@ TwoNa.Game.SetVehicleProperties = function(vehicle, props)
     end
 end
 
+TwoNa.Game.GetVehicleDamage = function(vehicle)
+    local damage = {}
+
+    damage["health"] = GetEntityHealth(vehicle)
+    damage["engineHealth"] = GetVehicleEngineHealth(vehicle)
+    damage["bodyHealth"] = GetVehicleBodyHealth(vehicle)
+    damage["petrolTankHealth"] = GetVehiclePetrolTankHealth(vehicle)
+    damage["dirt"] = GetVehicleDirtLevel(vehicle)
+    damage["headligths"] = {GetIsLeftVehicleHeadlightDamaged(vehicle), GetIsRightVehicleHeadlightDamaged(vehicle)}
+    damage["wheel"] = {}
+    damage["wheelBurst"] = {}
+    damage["door"] = {}
+
+    for i = 0, 5 do 
+        table.insert(damage.wheel, GetVehicleWheelHealth(vehicle, i))
+        table.insert(damage.wheelBurst, IsVehicleTyreBurst(vehicle, i, true))
+        table.insert(damage.door, IsVehicleDoorDamaged(vehicle, i))
+    end
+
+    return damage
+end
+
+TwoNa.Game.ApplyVehicleDamage = function(vehicle, damage)
+    SetEntityHealth(vehicle, damage.health) 
+    SetVehicleEngineHealth(vehicle, damage.engineHealth)
+    SetVehicleBodyHealth(vehicle, damage.bodyHealth)
+    SetVehiclePetrolTankHealth(vehicle, damage.petrolTankHealth)
+    SetVehicleDirtLevel(vehicle, damage.dirt)
+
+    for i = 0, 5 do 
+        SetVehicleWheelHealth(vehicle, i, damage.wheel[i])
+        SetVehicleWheelHealth(vehicle, i, damage.wheel[i])
+        if vehicle.wheelBurst[i] then 
+            SetVehicleTyreBurst(vehicle, i, true, 1000.0)
+        end
+        if vehicles.door[i] then 
+            SetVehicleDoorBroken(vehicle, i, true)
+        end
+    end
+end
+
 TwoNa.Draw3DText = function(x, y, z, scale, text) 
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
     local px,py,pz=table.unpack(GetGameplayCamCoords())
--- Config.lua
+++ Config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.MySQL = "mysql-async" -- Suports both mysql-async and oxmysql
+Config.MySQL = "oxmysql" -- Suports both mysql-async and oxmysql
 Config.Framework = "ESX" -- Suports both ESX and QB
